1bfe297fe3b1ff8a6901139e54b9949ae96d520c
Nuggetmeister <47368822+icravenuggets@users.noreply.github.com>
2019-02-26 14:18:54 -0600

    second work period

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -8,19 +8,26 @@
 // - Used on-screen text
 // - Imported and used different fonts for text
 // - Implemented Sound
+// - scalar on the cursor that is more satisfying to click on by changing size temporarily
 
 
 
 // Ideas to implement:
 // - Money falling from sky every click (figure out how to make images appear and disappear)
 // - Cha-ching sounds every click (that restart itself)
+// - automatic clicker that you can buy with clicks
+// - title screen
 // - Background music that loops
 // - Keyboard interaction to pause music, change volume, reset (clicks = 0)
 
 
 
 let clicks = 0;
-let money, scalar;
+let timeCounter = 0;
+let money, scalar, changingScalar;
+let workerAmount = 0;
+moneyFalling = false;
+let moneyFallingX, moneyFallingY;
 
 // Preloading the images, font(s), and sounds used
 function preload() {
@@ -32,47 +39,51 @@ function preload() {
   office = loadImage('assets/office.jpg');
   beach = loadImage('assets/beach.png');
   fontObelix = loadFont('assets/obelix.ttf');
-  // soundFormats('mp3');
-  // moneySound = loadSound('assets/moneySound.mp3');
-  // backgroundMusic = loadSound('assets/backgroundMusic.mp3');
+  soundFormats('mp3');
+  moneySound = loadSound('assets/moneySound.mp3');
+  backgroundMusic = loadSound('assets/backgroundMusic.mp3');
 }
 
 // Creating the canvas
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  scalar = windowWidth / 30;            // The scalar on the dollar sign cursor scales with window width
+  scalar = windowWidth / 30;            // The scalar scales with window width
+  changingScalar = windowWidth / 30;
+  backgroundMusic.setVolume(50);
+  backgroundMusic.play();
+
 }
 
 function draw() {
   // Background based on progress
-  if (clicks < 10) {
+  if (clicks < 100) {
     image(street, 0, 0, windowWidth, windowHeight);
     textSize(scalar);
     fill(255, 0, 0);
     textFont(fontObelix);
     text("Living on the streets", 50, 100);
   }
-  else if (clicks < 50) {
+  else if (clicks < 1000) {
     image(garage, 0, 0, windowWidth, windowHeight);
     textSize(scalar);
     fill(255, 0, 0);
     textFont(fontObelix);
     text("Working from a garage", 50, 100);
   }
-  else if (clicks < 100) {
+  else if (clicks < 10000) {
     image(room, 0, 0, windowWidth, windowHeight);
     textSize(scalar);
     fill(255, 0, 0);
     textFont(fontObelix);
     text("Decent work place", 50, 100);
   }
-  else if (clicks < 300) {
+  else if (clicks < 100000) {
     image(office, 0, 0, windowWidth, windowHeight);
     textSize(scalar);
     fill(255, 0, 0);
     textFont(fontObelix);
     text("High quality office", 50, 100);
-  }
+  } 
   else {
     image(beach, 0, 0, windowWidth, windowHeight);
     textSize(scalar);
@@ -82,19 +93,42 @@ function draw() {
   }
 
   // Dollar sign on cursor
-  image(dollarSign, mouseX - (scalar/2), mouseY - (scalar/2), scalar, scalar);
+  image(dollarSign, mouseX - (changingScalar/2), mouseY - (changingScalar/2), changingScalar, changingScalar);
+  if (changingScalar >= windowWidth/30) {
+    changingScalar -= 2;
+  }
   
   // Click counter on-screen
   textSize(scalar);
   fill(255)
   text(clicks, windowWidth - (windowWidth - 100), windowHeight - 200)
+
+  timeCounter += 1
+
+
+
+
+
+
+ // if (moneyFalling) {
+ //   image(money, moneyFallingX, moneyFallingY, scalar, scalar);
+ //   if (moneyFallingY > 0) {
+ //     moneyFallingY -= 1:
+ //   }
+ //   else {
+ //     moneyFalling = 0
+ //   }
+//  }
 }
 
-// Counting the number of clicks
+// What happens when you click the mouse
 function mousePressed() {
   clicks += 1;
-  // moneySound.setVolume(100);
-  // moneySound.play();
+  moneyFallingX = random(windowWidth)
+  moneyFalling = true;
+  changingScalar += 10;
+  moneySound.setVolume(100);
+  moneySound.play();
 }
 
 // The canvas size and cursor size are changed to fit the new window size
@@ -107,8 +141,11 @@ function windowResized() {
   else if (scalar > 150) {
     scalar = 150;
   }
+  changingScalar = windowWidth / 30;
+  if (changingScalar < 30) {                // Scalar size limits in case it gets too small or big
+    changingScalar = 30;
+  }
+  else if (changingScalar > 150) {
+    changingScalar = 150;
+  }
 }
-
-// function moneyFalling() {
-//   image(money, random(screenWidth), 0, scalar, scalar);
-// }
