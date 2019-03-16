9be514568070de07b41f5b780f66fab8c7afd116
Nuggetmeister <47368822+icravenuggets@users.noreply.github.com>
2019-03-16 14:13:12 -0600

    Completed Basic Version

--- State Variables/sketch.js
+++ State Variables/sketch.js
@@ -2,25 +2,47 @@
 // Alex Chen
 // Date
 //
+// State Variables usage:
+// - Game state, and other states for different menus (options and instructions)
+// - Different game state if it is an AI game
+// - Win state that changes if someone wins
+//
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-// - explain state variables and their use
-// - arrays
-// - simple ai
+// - Arrays
+//
+// Ideas for Stuff to Do:
+// - Simple and complex AI
+//    - Easy: Random
+//    - Medium: Tries to stay alive
+//    - Hard: With Algorithm
+// - Options menu
+//    - Sound On/Off
+//    - Volume Up/Down
+//    - Background Colour
+// - Instructions menu
+// - Aesthetics
+//    - Better Menus (With titles and stuff)
+//    - Better Cursor in menus
+// - Keyboard interaction
+// - Score board
+// - Music or sound effects
+// - Comments
+// - Organization into files
 
-let gameState = "mainMenu";
-let button, turn, blockSizeX, blockSizeY, clickSpotX, clickSpotY;
-let winState = 0;
+
+
+let button, turn, blockSizeX, blockSizeY, clickSpotX, clickSpotY, tilesCounter, winState, gameState;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   resizeScalars();
   textAlign(CENTER, CENTER);
   textSize(100);
-  createBoard();
   noCursor();
   turn = floor(random(1, 3));
   initiateBoard();
+  gameState = "mainMenu";
+  winState = 0;
 }
 
 function preload() {
@@ -45,12 +67,6 @@ function resizeScalars() {
   blockSizeY = windowHeight / 3;
 }
 
-
-// Pointer cursor
-
-function createBoard() {
-
-}
 
 function drawBoardLines() {
   background(255);
@@ -104,11 +120,14 @@ function instructionsMenu() {
 function game() {
   drawBoardLines();
   fillInBlocks();
+  checkForWinner();
+  isWinner();
+  checkForTie();
 }
 
 function drawCursor() {
   textAlign(CENTER, CENTER);
-  textSize(100);
+  textSize(windowWidth / 30);
   if (gameState != "game") {
     fill(255, 0, 0)
     ellipse(mouseX, mouseY, 10, 10)
@@ -126,6 +145,11 @@ function drawCursor() {
 
 
 function fillInBlocks() {
+  if (windowWidth > windowHeight)
+    textSize(windowHeight / 3);
+  else {
+    textSize(windowWidth / 3);
+  }
   for (let x = 0; x < board.length; x++) {
     for (let y = 0; y < board.length; y++) {
       if (board[x][y] === 1) {
@@ -141,19 +165,25 @@ function fillInBlocks() {
 
 function mousePressed() {
   if (gameState === "game") {
-    clickSpotX = floor(mouseX / blockSizeX);
-    clickSpotY = floor(mouseY / blockSizeY);
-    if (board[clickSpotX][clickSpotY] === 0) {
-      board[clickSpotX][clickSpotY] = turn;
-      if (turn === 1) {
-        turn = 2
-      }
-      else if (turn === 2) {
-        turn = 1
+    if (winState === 0) {
+      clickSpotX = floor(mouseX / blockSizeX);
+      clickSpotY = floor(mouseY / blockSizeY);
+      if (board[clickSpotX][clickSpotY] === 0) {
+        board[clickSpotX][clickSpotY] = turn;
+        if (turn === 1) {
+          turn = 2;
+        }
+        else if (turn === 2) {
+          turn = 1;
+        }
       }
     }
+    else if (winState > 0) {
+      initiateBoard();
+      winState = 0
+    }
   }
-  if (gameState === "mainMenu") {
+  else if (gameState === "mainMenu") {
     if (mouseIsPressed && mouseX < windowWidth / 2 + button.RadiusWidth && mouseX > windowWidth / 2 - button.RadiusWidth && mouseY < windowHeight / 2 + button.RadiusHeight && mouseY > windowHeight / 2 - button.RadiusHeight) {
       gameState = "game";
     }
@@ -161,67 +191,58 @@ function mousePressed() {
 }
 
 
+function checkForWinner() {
+  for (let checkingX = 0; checkingX < board.length; checkingX ++) {
+    if (board[checkingX][0] > 0 && board[checkingX][0] === board[checkingX][1] && board[checkingX][0] === board[checkingX][2]) {
+      winState = board[checkingX][0];
+    }
+  }
+  for (let checkingY = 0; checkingY < board.length; checkingY++) {
+    if (board[0][checkingY] > 0 && board[0][checkingY] === board[1][checkingY] && board[0][checkingY] === board[2][checkingY]) {
+      winState = board[0][checkingY];
+    }
+  }
+  if (board[0][0] > 0 && board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
+    winState = board[0][0];
+  }
+  else if (board[0][2] > 0 && board[0][2] === board[1][1] && board[0][2] == board[2][0]) {
+    winState = board[0][2];
+  }
+}
+
+
+function isWinner() {
+  if (winState > 0) {
+    textSize(windowWidth / 10);
+    fill(255, 0, 0);
+    text("Click to Reset", windowWidth / 2, windowHeight / 2 + (windowHeight / 5));
+  }
+  if (winState === 1) {
+    text("Player X Wins!", windowWidth / 2, windowHeight / 2);
+  }
+  else if (winState === 2) {
+    text("Player O Wins!", windowWidth / 2, windowHeight / 2);
+  }
+  else if (winState === 3) {
+    text("It's a Tie!", windowWidth / 2, windowHeight / 2);
+  }
+}
+
+
+function checkForTie() {
+  tilesCounter = 0;
+  for (let x = 0; x < board.length; x++) {
+    for (let y = 0; y < board.length; y++) {
+      if (board[x][y] > 0) {
+        tilesCounter ++;
+        if (tilesCounter >= 9 && winState === 0) {
+          winState = 3;
+        }
+      }
+    }
+  }
+}
 
 
 
 
-
-// 	winner();
-
-
-
-
-// function winner() {
-
-// 	for(let i = 0; i < tic.length; i++) {
-
-// 		winState = isWinner(tic[i][0], tic[i][1], tic[i][2]);
-
-// 		// print(winState);
-
-// 	}
-
-// 	if (winState > 0) {
-
-// 		if (winState === 1) {
-
-// 			textSize(10);
-
-// 			text("Player 1 wins!", width/2, height/2);
-
-// 			noLoop();
-
-// 		}
-
-// 	}
-
-// }
-
-
-// function isWinner(a, b, c) {
-
-// 	print(str(a) + str(b) + str(c));
-
-// 	if(a > 0 && b > 0 && c > 0) {
-
-// 		if (a === b && b === c) {
-
-// 			return a;
-
-// 		}
-
-// 		else {
-
-// 			return 0;
-
-// 		}
-
-// 	}
-
-// 	else {
-
-// 		return 0;
-
-// 	}
-
-// }
