0b85c6b2d62dffb31b50329eff26aef5608a5346
Nuggetmeister <47368822+icravenuggets@users.noreply.github.com>
2019-03-16 01:08:20 -0600

    Finished Board Stuff

--- State Variables/sketch.js
+++ State Variables/sketch.js
@@ -4,10 +4,12 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+// - explain state variables and their use
+// - arrays
+// - simple ai
 
 let gameState = "mainMenu";
-let button;
-let turn = 0;
+let button, turn, blockSizeX, blockSizeY, clickSpotX, clickSpotY;
 let winState = 0;
 
 function setup() {
@@ -17,6 +19,8 @@ function setup() {
   textSize(100);
   createBoard();
   noCursor();
+  turn = floor(random(1, 3));
+  initiateBoard();
 }
 
 function preload() {
@@ -36,7 +40,9 @@ function resizeScalars() {
     ScalarHeight: windowHeight / 5,
     RadiusWidth: windowWidth / 5 / 2,
     RadiusHeight: windowHeight / 5 / 2,
-  }
+  };
+  blockSizeX = windowWidth / 3;
+  blockSizeY = windowHeight / 3;
 }
 
 
@@ -47,11 +53,11 @@ function createBoard() {
 }
 
 function drawBoardLines() {
-	background(255);
-  line(windowWidth / 3, 0, windowWidth /3, windowHeight);
-  line(windowWidth - windowWidth / 3, 0, windowWidth - windowWidth / 3, windowHeight);
-  line(0, windowHeight / 3, windowWidth, windowHeight / 3);
-  line(0, windowHeight - windowHeight / 3, windowWidth, windowHeight - windowHeight / 3);
+  background(255);
+  line(blockSizeX, 0, blockSizeX, windowHeight);
+  line(windowWidth - blockSizeX, 0, windowWidth - blockSizeX, windowHeight);
+  line(0, blockSizeY, windowWidth, blockSizeY);
+  line(0, windowHeight - blockSizeY, windowWidth, windowHeight - blockSizeY);
 }
 
 
@@ -60,7 +66,6 @@ function draw() {
   background(255);
   pickingGameState();
   drawCursor();
-  console.log(gameState)
 }
 
 
@@ -79,13 +84,13 @@ function pickingGameState() {
   }
 }
 
+function initiateBoard() {
+  board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+}
+
 
 function mainMenu() {
   image(playButton, (windowWidth / 2) - button.RadiusWidth, (windowHeight / 2) - button.RadiusHeight, button.ScalarWidth, button.ScalarHeight);
-  if (mouseIsPressed && mouseX < windowWidth / 2 + button.RadiusWidth && mouseX > windowWidth / 2 - button.RadiusWidth && mouseY < windowHeight / 2 + button.RadiusHeight && mouseY > windowHeight / 2 - button.RadiusHeight) {
-    gameState = "game";
-  }
-
 }
 
 function optionsMenu() {
@@ -98,23 +103,60 @@ function instructionsMenu() {
 
 function game() {
   drawBoardLines();
+  fillInBlocks();
 }
 
 function drawCursor() {
+  textAlign(CENTER, CENTER);
+  textSize(100);
   if (gameState != "game") {
     fill(255, 0, 0)
-    noStroke()
     ellipse(mouseX, mouseY, 10, 10)
   }
   else if (gameState === "game") {
     fill(0)
-    text("X", mouseX, mouseY)
-  //   if (turn === 1) {
-  //     text("X", mouseX, mouseY);
-  //   }
-  //   if (turn === 2) {
-  //     text("O", mouseX, mouseY);
-  //   }
+    if (turn === 1) {
+      text("X", mouseX, mouseY);
+    }
+    if (turn === 2) {
+      text("O", mouseX, mouseY);
+    }
+  }
+}
+
+
+function fillInBlocks() {
+  for (let x = 0; x < board.length; x++) {
+    for (let y = 0; y < board.length; y++) {
+      if (board[x][y] === 1) {
+        text("X", x * blockSizeX + (blockSizeX / 2), y * blockSizeY + (blockSizeY / 2));
+      }
+      else if (board[x][y] === 2) {
+        text("O", x * blockSizeX + (blockSizeX / 2), y * blockSizeY + (blockSizeY / 2));
+      }
+    }
+  }
+}
+
+
+function mousePressed() {
+  if (gameState === "game") {
+    clickSpotX = floor(mouseX / blockSizeX);
+    clickSpotY = floor(mouseY / blockSizeY);
+    if (board[clickSpotX][clickSpotY] === 0) {
+      board[clickSpotX][clickSpotY] = turn;
+      if (turn === 1) {
+        turn = 2
+      }
+      else if (turn === 2) {
+        turn = 1
+      }
+    }
+  }
+  if (gameState === "mainMenu") {
+    if (mouseIsPressed && mouseX < windowWidth / 2 + button.RadiusWidth && mouseX > windowWidth / 2 - button.RadiusWidth && mouseY < windowHeight / 2 + button.RadiusHeight && mouseY > windowHeight / 2 - button.RadiusHeight) {
+      gameState = "game";
+    }
   }
 }
 
@@ -124,87 +166,9 @@ function drawCursor() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// let tic;
-
-
-// function draw() {
-
-// 	drawBoard();
-
-	
-
-// 	for(let i = 0; i < tic.length; i++) {
-
-// 		for(let j = 0; j < tic[i].length; j++) {
-
-// 			if (tic[i][j] === 1) {
-
-// 				text("X", i * 100 + 50, j * 100 + 50);
-
-// 			}
-
-// 			else if (tic[i][j] === 2) {
-
-// 				text("O", i * 100 + 50, j * 100 + 50);
-
-// 			}
-
-// 		}
-
-// 	}
-
-// }
-
-
-
-
-
-// function initBoard() {
-
-// 	tic = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
-
-// }
-
-
-// function mousePressed() {
-
-// 	let temp1 = Math.floor(mouseX/100);
-
-// 	let temp2 = Math.floor(mouseY/100);
-
-// 	if (tic[temp1][temp2] === 0) {
-
-// 		tic[temp1][temp2] = turn;
-
-// 		if (turn === 1) {
-
-// 			turn = 2;
-
-// 		}
-
-// 		else {
-
-// 			turn = 1;
-
-// 		}
-
-// 	}
-
 // 	winner();
 
-// }
+
 
 
 // function winner() {
