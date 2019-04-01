02a221571288904a7316e474b0e4870783d8dead
Nuggetmeister <47368822+icravenuggets@users.noreply.github.com>
2019-04-01 15:20:05 -0600

    Worked on ripple effect

---
+++ Ripple Effect/sketch.js
@@ -0,0 +1,111 @@
+// Ripple Effect
+// Alex
+
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+
+function draw() {
+  background(70, 140, 240);
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Ripple Effect
+// Alex
+
+// let rippleArray = [];
+// let newRipple;
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+// }
+
+
+// function mousePressed() {
+//   newRipple = {
+//     x: mouseX,
+//     y: mouseY,
+//     growth: 5,
+//     radius: 10,
+//     color: color(70, 90, 240),
+//   };
+//   rippleArray.push(newRipple); 
+// }
+
+
+// function draw() {
+//   background(70, 140, 240);
+
+//   newRipple.radius += newRipple.growth;
+//   fill(newRipple.color);
+//   noStroke();
+//   ellipse(newRipple.x, newRipple.y, newRipple.radius, newRipple.radius)
+
+
+  // for (let i = 0; i < ballArray.length; i++) {
+  //   fill(ballArray[i].color);
+  //   noStroke();
+  //   ellipse(ballArray[i].x, ballArray[i].y, ballArray[i].radius * 2, ballArray[i].radius * 2);
+  //   ballArray[i].x += ballArray[i].dx;
+  //   ballArray[i].y += ballArray[i].dy;
+  // }
+
+  
+// }
